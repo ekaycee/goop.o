02269b7710213ceafe17a00d628ea019a031c6ea
Kay Cee <8.kaycee@gmail.com>
2023-11-06 22:25:12 +0100

    feat: add book controller and common util

---
+++ server/api/v1/books/controllers/book.py
@@ -0,0 +1,16 @@
+
+#!/usr/bin/env python
+''' Book controller module '''
+from fastapi import Body, Request
+from fastapi.encoders import jsonable_encoder
+
+from common.models.book import Book
+
+
+class BookController:
+    ''' Book controller class '''
+
+    def create_book(cls, req: Request, book: Book = Body(...)):
+        ''' Create book method '''
+        book = jsonable_encoder(book)
+
